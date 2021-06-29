a9f1756491cd053c4ba6944373ccfc50d9a7d4c2
Simidzu Ay <simidzu2ay@protonmail.com>
2021-06-29 11:48:51 +0300

    All attachments

--- src/utils/get-attachments.util.ts
+++ src/utils/get-attachments.util.ts
@@ -7,23 +7,51 @@ import { MessagesMessageAttachment } from 'vk-io/lib/api/schemas/objects';
 export const getAttachments = (attachments: MessagesMessageAttachment[]) => {
     let attach: DAttachment[] = [];
 
-    for (const attachment of attachments) {
-        const att = attachmentRepository.create({});
+    for (const { type, ...obj } of attachments) {
+        const att = attachmentRepository.create({
+            type
+        });
+        const attachment = obj[type];
 
-        switch (attachment.type as AttachmentType) {
+        switch (type as AttachmentType) {
             case AttachmentType.PHOTO:
-                att.type = AttachmentType.PHOTO;
-                att.url = attachment.photo.sizes[attachment.photo.sizes.length - 1].url;
+                att.url = attachment.sizes[attachment.sizes.length - 1].url;
+                break;
+            case AttachmentType.AUDIO_MESSAGE:
+                att.url = attachment.link_mp3;
+                break;
+            case AttachmentType.WALL:
+                att.url = `https://vk.com/wall${attachment.from_id}_${attachment.id}`;
+                break;
+            case AttachmentType.MARKET:
+                att.url = `https://vk.com/product${attachment.owner_id}_${attachment.id}`;
                 break;
             case AttachmentType.VIDEO:
-                att.type = AttachmentType.VIDEO;
-                att.url = `https://vk.com/video${attachment.video.owner_id}_${attachment.video.id}_${attachment.video.access_key}`;
+            case AttachmentType.STORY:
+            case AttachmentType.POLL:
+                att.url = `https://vk.com/${type}${attachment.owner_id}_${attachment.id}`;
+                break;
+            case AttachmentType.STICKER:
+                att.url = attachment.images[attachment.images.length - 1].url;
+                break;
+            case AttachmentType.AUDIO:
+            case AttachmentType.DOCUMENT:
+            case AttachmentType.LINK:
+            case AttachmentType.DOCUMENT:
+            case AttachmentType.GRAFFITI:
+                att.url = attachment.url;
+                break;
+            case AttachmentType.WALL_REPLY:
+                att.url = `https://vk.com/wall/${attachment.owner_id}_${attachment.post_id}?reply=${attachment.id}`;
+                break;
+            case AttachmentType.GIFT:
+                att.url = attachment.thumb_256;
                 break;
             default:
-                Logger.warn('Неизвестный тип вложения', attachment);
+                Logger.warn('Неизвестный тип вложения', type, attachment);
         }
 
-        if (att.type) attach.push(att);
+        if (att.url) attach.push(att);
     }
 
     return attach;
