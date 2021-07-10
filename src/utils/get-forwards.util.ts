5d97de1677ca80e6ae86f3d6de1155817fe11927
Simidzu Ay <simidzu2ay@protonmail.com>
2021-07-10 23:16:32 +0300

    Fix text encryption in forward messages

--- src/utils/get-forwards.util.ts
+++ src/utils/get-forwards.util.ts
@@ -2,6 +2,8 @@ import { forwardRepository } from '../database';
 import { Forward } from '../database/forwards.entity';
 import { MessagesForeignMessage } from 'vk-io/lib/api/schemas/objects';
 import { getAttachments } from './get-attachments.util';
+import { AES } from 'crypto-js';
+import { cfg } from '../config';
 
 export const getForwards = (forwards: MessagesForeignMessage[], forwardTo?: Forward) => {
     const fwd: Forward[] = [];
@@ -11,7 +13,7 @@ export const getForwards = (forwards: MessagesForeignMessage[], forwardTo?: Forw
             forwardTo,
             attachments: forward.attachments ? getAttachments(forward.attachments) : [],
             fromId: forward.from_id,
-            text: forward.text
+            text: forward.text ? AES.encrypt(forward.text, cfg.encryption).toString() : undefined
         });
 
         f.forwards = getForwards(forward.fwd_messages ? forward.fwd_messages : [], f);
