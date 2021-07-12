4e83a8e263121493284752516b8310ffbefc5b37
Simidzu Ay <simidzu2ay@protonmail.com>
2021-07-12 19:49:37 +0300

    pretty table

--- cli/commands/get.message.ts
+++ cli/commands/get.message.ts
@@ -1,10 +1,16 @@
 import { Argument, Command, Option } from 'commander';
 import { FindManyOptions, In } from 'typeorm';
+import Table from 'cli-table3';
 import { AES, enc } from 'crypto-js';
 import { parseIntArrayUtil, parseIntUtil } from '../utils/parse-int.util';
 import { messagesRepository } from '../../dist/database';
 import { cfg } from '../../dist/config';
 import { Message } from '../../dist/database/message.entity';
+import WordWrap from 'wordwrap';
+
+const wrap = WordWrap(72, {
+    mode: 'hard'
+});
 
 export const getMessage = new Command('messages')
     .addArgument(new Argument('[ids...]', 'Message ids').argParser(parseIntArrayUtil))
@@ -17,17 +23,20 @@ export const getMessage = new Command('messages')
     .addOption(new Option('-d, --deleted', 'Get only deleted messages').default(false))
     .action(async (ids, commandOptions) => {
         let messages: Message[] | null;
+        const relations = ['attachments', 'replyTo', 'forwards'];
 
         if (ids.length)
             messages = await messagesRepository.find({
                 where: {
                     id: In(ids)
-                }
+                },
+                relations
             });
         else {
             if (process.env.NODE_ENV === 'dev') console.log(commandOptions);
 
             const options: FindManyOptions<Message> = {
+                relations,
                 take: commandOptions.count,
                 skip: commandOptions.offset,
                 where: {},
@@ -66,13 +75,38 @@ export const getMessage = new Command('messages')
             messages = await messagesRepository.find(options);
         }
 
-        if (messages?.length)
-            return console.table(
-                messages.map(m => ({
+        if (messages?.length) {
+            const table = new Table({
+                head: ['id', 'fromId', 'text', 'peerId', 'date', 'del', 'has']
+            });
+
+            const tMessages = messages.map(m => {
+                const has: string[] = [];
+
+                if (m.replyTo) has.push('reply');
+                if (m.attachments.length) has.push('attachments');
+                if (m.forwards.length) has.push('forwards');
+
+                return {
                     ...m,
-                    text: m.text ? AES.decrypt(m.text, cfg.encryption).toString(enc.Utf8) : null,
-                    date: m.date.toLocaleString('ru-RU')
-                }))
+                    text: m.text ? AES.decrypt(m.text, cfg.encryption).toString(enc.Utf8) : '',
+                    date: m.date.toLocaleString('ru-RU'),
+                    has
+                };
+            });
+
+            table.push(
+                ...tMessages.map(m => [
+                    m.id,
+                    m.fromId,
+                    wrap(m.text),
+                    m.peerId,
+                    m.date,
+                    m.isDeleted,
+                    m.has.length ? m.has.join('|') : 'none'
+                ])
             );
-        else throw new Error('No messages found');
+
+            return console.log(table.toString());
+        } else throw new Error('No messages found');
     });
